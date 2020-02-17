c2202b6b12c126891f90659fe4088caa06ac556c
esrakalkan <esrakalkan3000@gmail.com>
2020-02-17 20:43:53 +0300

    karakter hareketleri

---
+++ Bakis.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bakis : MonoBehaviour //karakterin gozlerinden etrafi görebilme
+{
+    private Vector2 mD;
+    private float inputH;
+    private float inputVMouse;
+    // Start is called before the first frame update
+
+
+    private Transform myBody;
+    void Start()
+    {
+        myBody = this.transform.parent.transform.parent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector2 mC = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        mD += mC;
+
+        this.transform.localRotation = Quaternion.AngleAxis(-mD.y, Vector3.right);
+        myBody.localRotation = Quaternion.AngleAxis(mD.x, Vector3.up);
+
+    }
+}
---
+++ CharacterScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterScript : MonoBehaviour
+{
+
+    public Animator anim;
+    private float inputV;
+    private float counter;
+    private bool sahne2, sahne3;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        counter = 0;
+        anim = GetComponent<Animator>();
+        sahne2 = true;
+        sahne3 = false;
+    }
+
+    // Update is called once per frame
+    void Update()  //sahne2 ve sahne3'e gecis yaparak farkli yukseklik seviyeleri denenmesi saglaniyor.
+    {
+        counter = counter + Time.deltaTime;
+
+        inputV = Input.GetAxis("Vertical");
+
+        anim.SetFloat("inputV", inputV);
+
+        transform.Translate(0, 0, inputV * Time.deltaTime * 5f);
+
+        if (counter >= 20 && sahne2 == true)
+        {
+            transform.position = new Vector3(2, 24, -10);
+            sahne2 = false;
+            sahne3 = true;
+        }
+        else if (counter >= 40 && sahne3 == true)
+        {
+            transform.position = new Vector3(144, 40, -55);
+            sahne3 = false;
+        }
+    }
+}
